3658bedb2bc84096881b770fdd260158b8739d2e
Ron MacNeil <macro@hotmail.com>
2017-11-19 00:12:35 +1000

    SlnModule: Add project files to graph and use for publish

--- produce/Modules/SlnModule.cs
+++ produce/Modules/SlnModule.cs
@@ -4,6 +4,7 @@ using MacroGuards;
 using MacroSln;
 using System.IO;
 using System.Linq;
+using System;
 
 namespace
 produce
@@ -21,32 +22,64 @@ Attach(ProduceRepository repository, Graph graph)
     Guard.NotNull(repository, nameof(repository));
     Guard.NotNull(graph, nameof(graph));
 
-    // Candidate .sln locations
+    // Solution paths
     // TODO Use patterns e.g. **/*.sln once supported
     var slnPaths = graph.List("sln-paths", 
         Path.Combine(repository.Path, ".nugit", repository.Name + ".sln"),
         Path.Combine(repository.Path, repository.Name + ".sln"));
 
-    // Candidate .sln files 
+    // Solution files 
     var slnFiles = graph.FileSet("sln-files");
     graph.Dependency(slnPaths, slnFiles);
 
-    // Path to selected .sln file
+    // Primary solution path
     var slnPath = graph.List("sln-path", _ => slnFiles.Files.Select(f => f.Path).Take(1));
     graph.Dependency(slnFiles, slnPath);
 
-    // Selected .sln file
+    // Primary solution file
     var slnFile = graph.FileSet("sln-file");
     graph.Dependency(slnPath, slnFile);
+
+    // Project paths
+    var slnProjPaths = graph.List("sln-proj-paths", _ =>
+        slnFile.Files.Any()
+            ? new VisualStudioSolution(slnFile.Files.Single().Path).ProjectReferences.Select(r => r.GetProject().Path)
+            : Enumerable.Empty<string>());
+    graph.Dependency(slnFile, slnProjPaths);
+
+    // Project files
+    var slnProjFiles = graph.FileSet("sln-proj-files");
+    graph.Dependency(slnProjPaths, slnProjFiles);
+
+    // Primary project path
+    var slnProjPath = graph.List("sln-proj-path", _ =>
+        slnProjFiles.Files
+            .Select(f => f.Path)
+            .Where(p =>
+                string.Equals(
+                    Path.GetFileNameWithoutExtension(p),
+                    repository.Name,
+                    StringComparison.OrdinalIgnoreCase))
+            .Take(1));
+    graph.Dependency(slnProjFiles, slnProjPath);
+
+    // Primary project file
+    var slnProjFile = graph.FileSet("sln-proj-file");
+    graph.Dependency(slnProjPath, slnProjFile);
+
+    // Publish destination
+    var slnPublishPath = graph.List("sln-publish-path", repository.GetWorkSubdirectory("sln-publish"));
 
     var slnBuild = graph.Command("sln-build", _ =>
         Sln(repository, "build", slnFile.Files.SingleOrDefault()?.Path));
     graph.Dependency(slnFile, slnBuild);
+    graph.Dependency(slnProjFiles, slnBuild);
     graph.Dependency(slnBuild, graph.Command("build"));
 
     var slnRebuild = graph.Command("sln-rebuild", _ =>
         Sln(repository, "rebuild", slnFile.Files.SingleOrDefault()?.Path));
     graph.Dependency(slnFile, slnRebuild);
+    graph.Dependency(slnProjFiles, slnRebuild);
     graph.Dependency(slnRebuild, graph.Command("rebuild"));
 
     var slnClean = graph.Command("sln-clean", _ =>
@@ -54,14 +87,17 @@ Attach(ProduceRepository repository, Graph graph)
     graph.Dependency(slnFile, slnClean);
     graph.Dependency(slnClean, graph.Command("clean"));
 
-    var slnPublishPath = graph.List("sln-publish-path", repository.GetWorkSubdirectory("sln-publish"));
-    graph.Dependency(slnPublishPath, graph.Command("publish"));
-
     var slnPublish = graph.Command("sln-publish", _ =>
-        Publish(repository, slnPath.Values.SingleOrDefault(), slnPublishPath.Values?.Single()));
+        Publish(
+            repository,
+            slnFile.Files.SingleOrDefault()?.Path,
+            slnProjFile.Files.SingleOrDefault()?.Path,
+            slnPublishPath.Values.Single()));
+    graph.Dependency(slnFile, slnPublish);
+    graph.Dependency(slnProjFile, slnPublish);
     graph.Dependency(slnPublishPath, slnPublish);
-    graph.Dependency(slnBuild, slnPublish);
     graph.Dependency(slnPublish, graph.Command("publish"));
+    graph.Dependency(slnPublishPath, graph.Command("publish"));
 }
 
 
@@ -82,10 +118,13 @@ Sln(ProduceRepository repository, string command, string slnPath)
 
 
 static void
-Publish(ProduceRepository repository, string slnPath, string destinationPath)
+Publish(ProduceRepository repository, string slnPath, string projPath, string destinationPath)
 {
     Guard.NotNull(repository, nameof(repository));
     if (slnPath == null) return;
+    if (projPath == null) return;
+
+    var projName = Path.GetFileNameWithoutExtension(projPath);
 
     using (LogicalOperation.Start("Publishing " + slnPath + " to " + destinationPath))
     {
@@ -98,8 +137,7 @@ Publish(ProduceRepository repository, string slnPath, string destinationPath)
 
         if (ProcessExtensions.Execute(
             true, true, repository.Path, "cmd", "/c",
-            "sln", "publish", slnPath, destinationPath
-        ) != 0)
+            "sln", "publish", slnPath, projName, destinationPath ) != 0)
             throw new UserException("Failed");
     }
 }
